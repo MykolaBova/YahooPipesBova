363a0d93374a441bc80d7ab54496d994504da81b
m.bova <m.bova@levi9.com>
2015-09-23 16:34:50 +0300

    Some code refactoring

--- Angular/YahooPipesBovaAngular/app/controllers/newsCtrl.js
+++ Angular/YahooPipesBovaAngular/app/controllers/newsCtrl.js
@@ -406,57 +406,11 @@
                 }
             };
 
-        vm.getItems = function() {
-            return vm.news.value.items;
-        };
-
-        vm.getCount = function() {
-            return vm.news.count;
-        };
-
-/*
-        vm.news = [
-            {" productId": 1,
-                "productName": "Leaf Rake",
-                "productCode": "GDN-0011",
-                "releaseDate": "March 19, 2009",
-                "description": "Leaf rake with 48-inch handle.",
-                "cost": 9.00,
-                "price": 19.95,
-                "category": "garden",
-                "tags": [ "leaf", "tool" ],
-                "imageUrl": "http://openclipart.org/image/300px/svg_to_png/26215/Anonymous_Leaf_Rake.png"
-            },
-            {" productId": 100,
-                "productName": "Leaf Rake 8",
-                "productCode": "GDN-0017",
-                "releaseDate": "March 4, 2009",
-                "description": "Leaf ))) rake with 48-inch handle.",
-                "cost": 19.00,
-                "price": 119.95,
-                "category": "garden",
-                "tags": [ "leaf" ],
-                "imageUrl": "http://openclipart.org/image/300px/svg_to_png/26215/Anonymous_Leaf_Rake.png"
-            },            
-            {
-                "productId": 5,
-                "productName": "Hammer",
-                "productCode": "TBX-0048",
-                "releaseDate": "May 21, 2013",
-                "description": "Curved claw steel hammer",
-                "cost": 1.00,
-                "price": 8.99,
-                "category": "toolbox",
-                "tags": ["tool"],
-                "imageUrl": "http://openclipart.org/image/300px/svg_to_png/73/rejon_Hammer.png"
-            }];
-*/
-
-
         vm.showImage = false;
 
         vm.toggleImage = function() {
             vm.showImage = !vm.showImage;
         }
+
     }
 }());
