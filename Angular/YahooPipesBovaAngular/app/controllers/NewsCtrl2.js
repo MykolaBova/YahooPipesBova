081f32c74e3cece0e996eed9ccf1a8d40be51380
m.bova <m.bova@levi9.com>
2015-09-24 13:24:53 +0300

    First steps towards moving logic to service (working)

--- Angular/YahooPipesBovaAngular/app/controllers/NewsCtrl2.js
+++ Angular/YahooPipesBovaAngular/app/controllers/NewsCtrl2.js
@@ -3,9 +3,25 @@
     angular
         .module("news")
         .controller("NewsCtrl2",
-            NewsCtrl2);
+            ["Serv", NewsCtrl2]);
 
-    function NewsCtrl2($scope, $http) {
+    function NewsCtrl2(Serv) {
+
+        var vm = this;
+        vm.errors = [];
+
+        Serv
+            .getNews()
+            .then(
+                function(response){
+                    //vm.pipe = response.data.value;
+                    vm.news = response.data;
+                },
+                function(){
+                    vm.errors.push("Sorry, error retrieving data. Please, try again later.");
+                });
+
+/*
         var vm = this;
 
         $http.jsonp("http://pipes.yahoo.com/pipes/pipe.run?_id=e9a2e77dffb3205d035c4e311d77bbe6&_render=json&_callback=JSON_CALLBACK").
@@ -17,5 +33,8 @@
         error(function (data) {
             $scope.data = "Request failed";
         });
+        */
+
+
     }
 }());
