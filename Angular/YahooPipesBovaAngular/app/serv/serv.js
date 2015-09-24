80487e4c84605f8f05109856859782234a5924c7
m.bova <m.bova@levi9.com>
2015-09-24 13:40:37 +0300

    First steps towards moving logic to service (working)

--- Angular/YahooPipesBovaAngular/app/serv/serv.js
+++ Angular/YahooPipesBovaAngular/app/serv/serv.js
@@ -1,9 +1,9 @@
 (function () {
     "use strict";
     angular.module("news")
-        .factory("Serv", ["$http", nasaNewsSource]);
+        .factory("Serv", ["$http", jsonpService]);
 
-    function nasaNewsSource($http){
+    function jsonpService($http){
         var PIPE_TMPLT_URL = "http://pipes.yahoo.com/pipes/pipe.run?_id=e9a2e77dffb3205d035c4e311d77bbe6&_render=json";
 
         var getNews = function(){
