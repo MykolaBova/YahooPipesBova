49b25e747b0ca2e0604aa319fb2df9aea99cf8ec
m.bova <m.bova@levi9.com>
2015-09-24 13:35:13 +0300

    First steps towards moving logic to service (working)

--- Angular/YahooPipesBovaAngular/app/controllers/NewsCtrl2.js
+++ Angular/YahooPipesBovaAngular/app/controllers/NewsCtrl2.js
@@ -14,27 +14,10 @@
             .getNews()
             .then(
                 function(response){
-                    //vm.pipe = response.data.value;
                     vm.news = response.data;
                 },
                 function(){
                     vm.errors.push("Sorry, error retrieving data. Please, try again later.");
                 });
-
-/*
-        var vm = this;
-
-        $http.jsonp("http://pipes.yahoo.com/pipes/pipe.run?_id=e9a2e77dffb3205d035c4e311d77bbe6&_render=json&_callback=JSON_CALLBACK").
-        success(function(data) {
-            //console.log(data);
-            vm.news = data;
-            //$scope.data = data;
-        }).
-        error(function (data) {
-            $scope.data = "Request failed";
-        });
-        */
-
-
     }
 }());
