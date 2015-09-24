11f4e6aad8e5dc2e8cf21c5e5d13b5a091a86938
m.bova <m.bova@levi9.com>
2015-09-24 13:34:51 +0300

    First steps towards moving logic to service (working)

--- Angular/YahooPipesBovaAngular/app/serv/serv.js
+++ Angular/YahooPipesBovaAngular/app/serv/serv.js
@@ -12,40 +12,4 @@
 
         return {getNews : getNews};
     }
-
-
-/*        .service('MyService', function ($http) {
-            //this.square = function (a) { return a*a};
-
-            var ser = this;
-
-            ser.sendJson = function(callback) {
-                ser.callback = callback;
-                $http.jsonp("http://pipes.yahoo.com/pipes/pipe.run?_id=e9a2e77dffb3205d035c4e311d77bbe6&_render=json&_callback=JSON_CALLBACK").
-                success(function (data) {
-                    console.log("----");
-                    console.log(data);
-                    console.log("----");
-                    console.log("----");
-                    console.log(callback);
-                    console.log("----");
-
-
-
-                    //ser.news = data;
-
-                    //ser.callback(data);
-                }).
-                error(function (data) {
-                    console.log("Request failed");
-                });
-            };
-
-            return {
-                sendJson: ser.sendJson
-            }
-
-        });*/
-
-
 }());
